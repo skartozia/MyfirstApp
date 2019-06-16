dc37a2940faf99be8b6b4ab4163cdcd5db419a96
soso kartozia <51861539+skartozia@users.noreply.github.com>
2019-06-17 01:22:38 +0400

    done Add New/Edit form

--- pages/index.vue
+++ pages/index.vue
@@ -503,156 +503,138 @@
       </th>
     </tr>
   </tfoot>
-</table> -->
+    </table>-->
 
     <div class="AddNew">
-			<form>
-				<fieldset>
-					<legend>Add New / Edit</legend>
-					<div class="AddNewAll">
-						<input type="text" placeholder="სახელი">
-						<input type="text" placeholder="გვარი">
-						<select name="city" id="city">
-							<option selected value disabled>მოქალაქეობა</option>
-							<option value="tbilisi">საქართველო</option>
-							<option value="nokalakevi">კანადა</option>
-						</select>
-						<br>
-						<select name="city" id="city">
-							<option selected value disabled>სქესი</option>
-							<option value="tbilisi">მამრობითი</option>
-							<option value="nokalakevi">მდედრობითი</option>
-						</select>
-						<input type="text" placeholder="პირადი ნომერი">
-						<div class="calimg">
-							<input type="text" placeholder="დაბადების თარიღი">
-							<img src="calendar.svg" alt="calendar">
-						</div>
-						<div class="calimg">
-							<input type="text" placeholder="გაცემის თარიღი">
-							<img src="calendar.svg" alt="calendar">
-						</div>
-						<div class="calimg">
-							<input type="text" placeholder="მოქმედების ვადა">
-							<img src="calendar.svg" alt="calendar">
-						</div>
-						<select name="city" id="city" placeholder="დაბადების ადგილი">
-							<option selected value disabled>დაბადების ადგილი</option>
-							<option value="tbilisi">თბილისი</option>
-							<option value="nokalakevi">ნოქალაქევი</option>
-							<option value="goresha">ღორეშა</option>
-							<option value="sxva">სხვას</option>
-						</select>
-						<input type="text" placeholder="ბარათის No"><br>
-						<button class="btn_create_edit">Create/Edit</button>
-					</div>
-				</fieldset>
-			</form>
-		</div>
-
-
-
-
-
+      <form>
+        <fieldset>
+          <legend>Add New / Edit</legend>
+          <div class="AddNewAll">
+            <input type="text" placeholder="სახელი">
+            <input type="text" placeholder="გვარი">
+            <select name="city" id="city">
+              <option selected value disabled>მოქალაქეობა</option>
+              <option value="tbilisi">საქართველო</option>
+              <option value="nokalakevi">კანადა</option>
+            </select>
+            <br>
+            <select name="city" id="city">
+              <option selected value disabled>სქესი</option>
+              <option value="tbilisi">მამრობითი</option>
+              <option value="nokalakevi">მდედრობითი</option>
+            </select>
+            <input type="text" placeholder="პირადი ნომერი">
+            <div class="calimg">
+              <input type="text" placeholder="დაბადების თარიღი">
+              <img src="calendar.svg" alt="calendar">
+            </div>
+            <div class="calimg">
+              <input type="text" placeholder="გაცემის თარიღი">
+              <img src="calendar.svg" alt="calendar">
+            </div>
+            <div class="calimg">
+              <input type="text" placeholder="მოქმედების ვადა">
+              <img src="calendar.svg" alt="calendar">
+            </div>
+            <select name="city" id="city" placeholder="დაბადების ადგილი">
+              <option selected value disabled>დაბადების ადგილი</option>
+              <option value="tbilisi">თბილისი</option>
+              <option value="nokalakevi">ნოქალაქევი</option>
+              <option value="goresha">ღორეშა</option>
+              <option value="sxva">სხვას</option>
+            </select>
+            <input type="text" placeholder="ბარათის No">
+            <br>
+            <div class="d-flex justify-content-end">
+              <button class="btn btn-secondary">Create/Edit</button>
+            </div>
+          </div>
+        </fieldset>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-};
+export default {};
 
 // $(document).ready(function () {
 //   $('#dtBasicExample').DataTable();
 //   $('.dataTables_length').addClass('bs-select');
 // });
-
 </script>
 
 <style scoped>
-.btn {
-  outline:none !important;
-
-}
-.btn_btn{
-	margin:10px;
-	background-color: #fff;
-	border:1px solid black;
-	border-radius: 5px;
-	outline:  none;
-	line-height: 1.5;
-	font-size: 16px;
+.btn_btn {
+  margin: 10px;
+  background-color: #fff;
+  border: 1px solid black;
+  border-radius: 5px;
+  outline: none;
+  line-height: 1.5;
+  font-size: 16px;
 }
 
-.btn_btn:hover{
-	background-color: #c0c0c0;
-	color: white;
+.btn_btn:hover {
+  background-color: #c0c0c0;
+  color: white;
 }
 
-.btn_btn:active{
-	background-color: black;
-	color:white;
+.btn_btn:active {
+  background-color: black;
+  color: white;
 
-	transition: all 0.5s ease;
+  transition: all 0.5s ease;
 }
 
-
-fieldset{
-	margin:10px;
-	padding-bottom: 30px;
+fieldset {
+  margin: 10px;
+  padding-bottom: 30px;
 }
 
-input, select{
-	margin:0 50px 10px 0;
-	border:2px solid black;
-	outline:none;
-	width: 250px;
-	padding: 5px;
-	box-sizing: border-box;
+input,
+select {
+  margin: 0 50px 10px 0;
+  border: 2px solid black;
+  outline: none;
+  width: 250px;
+  padding: 5px;
+  box-sizing: border-box;
 }
 
-select{
-  
-	padding: 0 0 5px 0;
-	
-	width: 250px;
+select {
+  padding: 0 0 5px 0;
+
+  width: 250px;
 }
 
-.AddNew{
-	width: 300px;
-	padding-bottom: 30px;
-	position: relative;
+.AddNew {
+  width: 300px;
+  padding-bottom: 30px;
+  position: relative;
 }
 
-.btn_create_edit{
-	position: absolute;
-	right: 0px;
+/* .btn_create_edit {
+  position: absolute;
+  right: 10px;
+} */
+
+.calimg input {
+  width: 210px;
+}
+.calimg{
+  position: relative;
+}
+.calimg img {
+  width: 40px;
+  position: absolute;
+  top: 0;
+  right:0;
+  cursor: pointer;
 }
 
-
-
-.calimg input{
-	width: 220px;
+.AddNewAll {
+  margin: 10px 0 0 30px;
 }
 
-.input img{
-position: absolute;
-z-index:10;
-left: 50px;
-	width: 30px;
-}
-
-.calimg img{
-	width: 30px;
-	position: absolute;
-	right: -5px;
-}
-
-.AddNewAll{
-	margin:10px 0 0 30px;
-	
-}
-
-.calimg img{
-  cursor:pointer;
-}
 </style>
